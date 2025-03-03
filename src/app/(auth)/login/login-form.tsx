c6889d7560298172d7760253012e72cda28e431c
fawadaslam77 <fwadaslam77@gmail.com>
2025-03-03 06:57:18 +0500

    Fix login form

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -38,60 +38,37 @@ export function LoginForm({
         console.log(values)
     }
     return (
-        // <Form {...form}>
-        //             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        //                 <div className="flex flex-col items-center gap-2 text-center">
-        //                     <h1 className="text-2xl font-bold">Reset password</h1>
-        //                     <p className="text-balance text-sm text-muted-foreground">
-        //                         Enter a strong password
-        //                     </p>
-        //                 </div>
-        //                 <RenderInput form={form} name="password" label="Password" />
-        //                 <RenderInput form={form} name="c_password" label="Confirm Password" />
-        //                 <Button type="submit" className="w-full">
-        //                     Reset Password
-        //                 </Button>
-
-        //                 <div className="text-center text-sm">
-        //                     Go back to login? &nbsp;
-        //                     <Link href="/login" className="underline underline-offset-4">
-        //                         Login
-        //                     </Link>
-        //                 </div>
-        //             </form>
-        //         </Form>
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className={cn("flex flex-col gap-6", className)} {...props}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={cn("flex flex-col space-y-4 ", className)} {...props}>
                 <div className="flex flex-col items-center gap-2 text-center">
                     <h1 className="text-2xl font-bold">Login</h1>
                     <small className="text-balance text-sm text-muted-foreground">
                         Enter your credentials below to login to your account
                     </small>
                 </div>
-                <div className="grid gap-6">
-                    <RenderInput form={form} name="email" label="Email" />
-                    <RenderInput form={form} name="password" label="Password" />
-                    <div className="flex items-center">
-                        <Link
-                            href="/forgot-password"
-                            className="ml-auto text-sm underline-offset-4 hover:underline"
-                        >
-                            Forgot your password?
-                        </Link>
-                    </div>
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
-                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                        <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                            Or continue with
-                        </span>
-                    </div>
-                    <Button disabled variant="outline" className="w-full">
-                        <Image src="/svgs/google.svg" width={20} height={20} alt="google" />
-                        Login with Google
-                    </Button>
+
+                <RenderInput form={form} name="email" label="Email" />
+                <RenderInput form={form} name="password" label="Password" />
+                <div className="flex items-center">
+                    <Link
+                        href="/forgot-password"
+                        className="ml-auto text-sm hover:underline"
+                    >
+                        Forgot your password?
+                    </Link>
                 </div>
+                <Button type="submit" className="w-full">
+                    Login
+                </Button>
+                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        or
+                    </span>
+                </div>
+                <Button disabled variant="outline" className="w-full">
+                    <Image src="/svgs/google.svg" width={20} height={20} alt="google" />
+                    Login with Google
+                </Button>
                 <div className="text-center text-sm">
                     Don&apos;t have an account?{" "}
                     <Link href="/sign-up" className="underline underline-offset-4">
--- src/app/(auth)/sign-up/signup-form.tsx
+++ src/app/(auth)/sign-up/signup-form.tsx
@@ -1,58 +1,82 @@
+'use client'
+
+import React from "react"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Image from "next/image"
 import Link from "next/link"
+
+import Image from "next/image"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form } from "@/components/ui/form"
+import { RenderInput } from "@/components/form-components/input"
+import { useForm } from "react-hook-form"
+
+
+const formSchema = z.object({
+    password: z.string().min(5, { message: "Password must be at least 5 characters." }),
+    email: z.coerce.string().email({ message: "Must be a valid email." }).min(5, { message: "Email is required." }),
+})
+
 
 export function SignupForm({
     className,
     ...props
 }: React.ComponentPropsWithoutRef<"form">) {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
     return (
-        <form className={cn("flex flex-col gap-6", className)} {...props}>
-            <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Register a new account</h1>
-                <p className="text-balance text-sm text-muted-foreground">
-                    Enter your details below to signup
-                </p>
-            </div>
-            <div className="grid gap-6">
-                <div className="grid gap-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={cn("flex flex-col space-y-4 ", className)} {...props}>
+                <div className="flex flex-col items-center gap-2 text-center">
+                    <h1 className="text-2xl font-bold">Signup</h1>
+                    <small className="text-balance text-sm text-muted-foreground">
+                        Enter your information to create your account
+                    </small>
                 </div>
-                <div className="grid gap-2">
-                    <div className="flex items-center">
-                        <Label htmlFor="password">Password</Label>
-                        <Link
-                            href="/forgot-password"
-                            className="ml-auto text-sm underline-offset-4 hover:underline"
-                        >
-                            Forgot your password?
-                        </Link>
-                    </div>
-                    <Input id="password" type="password" required />
+
+                <RenderInput form={form} name="email" label="Email" />
+                <RenderInput form={form} name="password" label="Password" />
+                <div className="flex items-center">
+                    <Link
+                        href="/forgot-password"
+                        className="ml-auto text-sm hover:underline"
+                    >
+                        Forgot your password?
+                    </Link>
                 </div>
                 <Button type="submit" className="w-full">
-                    Sign up
+                    Submit
                 </Button>
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                        Or continue with
+                        or
                     </span>
                 </div>
-                <Button variant="outline" className="w-full">
+                <Button disabled variant="outline" className="w-full">
                     <Image src="/svgs/google.svg" width={20} height={20} alt="google" />
-                    Continue with Google
+                    Login with Google
                 </Button>
-            </div>
-            <div className="text-center text-sm">
-                Already have an account?{" "}
-                <Link href="/login " className="underline underline-offset-4">
-                    Login
-                </Link>
-            </div>
-        </form>
+                <div className="text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link href="/sign-up" className="underline underline-offset-4">
+                        Sign up
+                    </Link>
+                </div>
+            </form>
+        </Form>
     )
 }
